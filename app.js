468f64f1c94ca53a31b759769ddbda656f5ae4fe
damidaa <dami.dataa@gmail.com>
2023-09-04 16:58:30 +0900

    make register

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const app = express()
 
 const routing = require("./routes/routing")
 const damirouting = require("./routes/damirouting")
-
+const register_routing = require("./routes/register_routing")
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
 app.use('/', routing)
 app.use('/dami',damirouting)
-
+app.use('/register',register_routing)
 
 app.set('views', path.resolve(__dirname + '/views'))
 app.set('view engine','ejs')
